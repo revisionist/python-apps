a56f58715e417220bae589b73da15ec6385612d3
David Goddard <dmg@parse.net>
2024-01-24 19:56:25 +0100

    Change formatting for consistency

--- flask/sposs/authentication.py
+++ flask/sposs/authentication.py
@@ -1,5 +1,5 @@
 # Copyright 2023-2024 David Goddard.
-
+#
 # Except where otherwise noted, this software is licensed under the Apache
 # License, Version 2.0 (the "License"); you may not use this file except in
 # compliance with the License. You may obtain a copy of the License at:
